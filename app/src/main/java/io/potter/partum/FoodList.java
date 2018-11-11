5bd96e632ad778d0fa94a12d9ea343f3e1decb0d
PerezArturo <prz.arturo@gmail.com>
2018-11-11 09:11:26 -0800

    2do(SearchBar)

--- app/src/main/java/io/potter/partum/FoodList.java
+++ app/src/main/java/io/potter/partum/FoodList.java
@@ -1,17 +1,27 @@
 package io.potter.partum;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.potter.partum.Common.Common;
 import io.potter.partum.Interface.ItemClickListener;
@@ -28,9 +38,13 @@ public class FoodList extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference foodlist;
 
-    String categoryId="";
+    String categoryId = "";
 
-    FirebaseRecyclerAdapter<Food,FoodViewHolder> adapter;
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> adapter;
+
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> searchadapter;
+    List<String> suggestList = new ArrayList<>();
+    MaterialSearchBar materialSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +59,103 @@ public class FoodList extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        if(getIntent() != null){
-            categoryId= getIntent().getStringExtra("CategoryId");
+        if (getIntent() != null) {
+            categoryId = getIntent().getStringExtra("CategoryId");
         }
-        if(!categoryId.isEmpty() && categoryId!=null){
-                loadListFood(categoryId);
+        if (!categoryId.isEmpty() && categoryId != null) {
+            loadListFood(categoryId);
 
         }
+
+        materialSearchBar = findViewById(R.id.searchBar);
+        materialSearchBar.setHint("Ingresa tu comida");
+        loadSuggest();
+        materialSearchBar.setLastSuggestions(suggestList);
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                List<String> suggest = new ArrayList<String>();
+                for (String search : suggestList) {
+                    if (search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+                materialSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                if (!enabled)
+                    recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearching(text);
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
+    }
+
+    private void startSearching(CharSequence text) {
+        searchadapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(
+                Food.class,
+                R.layout.food_item,
+                FoodViewHolder.class,
+                foodlist.orderByChild("Name").equalTo(text.toString())
+        ) {
+            @Override
+            protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
+                viewHolder.txtFoodName.setText(model.getName());
+                Picasso.get().load(model.getImage())
+                        .into(viewHolder.imageFoodView);
+                final Food local = model;
+
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Intent details = new Intent(FoodList.this, FoodDetail.class);
+                        details.putExtra("FoodId", searchadapter.getRef(position).getKey());
+                        startActivity(details);
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(searchadapter);
+    }
+
+    private void loadSuggest() {
+        foodlist.orderByChild("MenuId").equalTo(categoryId)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                            Food item = postSnapshot.getValue(Food.class);
+                            suggestList.add(item.getName());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     private void loadListFood(String categoryId) {
@@ -63,14 +167,14 @@ public class FoodList extends AppCompatActivity {
             protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
                 viewHolder.txtFoodName.setText(model.getName());
                 Picasso.get().load(model.getImage())
-                .into(viewHolder.imageFoodView);
+                        .into(viewHolder.imageFoodView);
                 final Food local = model;
 
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Intent details = new Intent(FoodList.this,FoodDetail.class);
-                        details.putExtra("FoodId",adapter.getRef(position).getKey());
+                        Intent details = new Intent(FoodList.this, FoodDetail.class);
+                        details.putExtra("FoodId", adapter.getRef(position).getKey());
                         startActivity(details);
                     }
                 });
